d06e9e244180d95d3853146a12b5b4f1ad8cfe68
nourahosam <34795230+nourahosam@users.noreply.github.com>
2022-05-15 00:04:21 +0200

    First Commit

---
+++ src/test/java/java/springboot/CrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package java.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
